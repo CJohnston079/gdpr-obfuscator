c7ac655ddbaf16d348da0717e56cf91563cce4fe
Chris <chris79gp@outlook.com>
2024-05-24 12:19:09 +0100

    refactor: refactor handle_csv to use list comprehension

--- src/file_handlers/handle_csv.py
+++ src/file_handlers/handle_csv.py
@@ -3,5 +3,4 @@ import csv
 
 def handle_csv(csv_file):
     with open(csv_file, 'r', newline='') as file:
-        csv_reader = csv.DictReader(file)
-        return [dict(row) for row in csv_reader]
+        return [dict(row) for row in csv.DictReader(file)]
