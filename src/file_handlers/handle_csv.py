2288944a4651c1a6300c69a1132a2dc4e198c6b7
Chris <chris79gp@outlook.com>
2024-05-24 11:25:37 +0100

    feat: add handle_csv() handle_csv() takes "csv_file" as an argument, which is the path to a csv file handle_csv() returns the csv file as a list of dictionaries if passed an empty file, handle_csv will return an empty list if passed an invalid file, handle_csv will raise a FileNotFoundError

--- src/file_handlers/handle_csv.py
+++ src/file_handlers/handle_csv.py
@@ -1,2 +1,12 @@
-def handle_csv():
-    pass
+import csv
+
+
+def handle_csv(csv_file):
+    data = []
+
+    with open(csv_file, 'r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(dict(row))
+
+    return data
