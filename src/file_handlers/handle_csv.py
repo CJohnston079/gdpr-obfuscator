6a5a13ecfcd36f434403ef3c072c3cc3b9d466af
Chris <chris79gp@outlook.com>
2024-05-24 12:12:00 +0100

    test: refactor test_handle_csv to enclose all tests in TestHandleCSV class replace context manager with patch decorator for all tests reformat samepl_csv_content to ensure it is pep8 compliant add main guard to test_handle_csv to control execution of script

--- src/file_handlers/handle_csv.py
+++ src/file_handlers/handle_csv.py
@@ -2,11 +2,6 @@ import csv
 
 
 def handle_csv(csv_file):
-    data = []
-
     with open(csv_file, 'r', newline='') as file:
         csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            data.append(dict(row))
-
-    return data
+        return [dict(row) for row in csv_reader]
