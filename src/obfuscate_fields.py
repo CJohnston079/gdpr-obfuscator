16aba85297c2f997a892985b3818ff2bfc7f26a1
Chris <chris79gp@outlook.com>
2024-05-25 00:33:35 +0100

    style: split dictionary comprehension over two lines to ensure pep8 compliance

--- src/obfuscate_fields.py
+++ src/obfuscate_fields.py
@@ -16,6 +16,9 @@ def obfuscate_fields(data, fields):
         replaced with '***'.
     """
     return [
-        {key: '***' if key in fields else value for key, value in record.items()}
+        {
+            key: '***' if key in fields else value
+            for key, value in record.items()
+        }
         for record in data
     ]
