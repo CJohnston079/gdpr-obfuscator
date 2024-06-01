d0e5f7f44848dc4c79973b6bac42fe5d97df012f
Chris <chris79gp@outlook.com>
2024-06-01 19:53:16 +0100

    docs: add docstring to serialise_dicts

--- src/serialise_dicts.py
+++ src/serialise_dicts.py
@@ -3,6 +3,15 @@ import pickle
 
 
 def serialise_dicts(dicts):
+    """
+    Serialize a list of dictionaries using pickle.
+
+    Args:
+        dicts (list of dict): A list of dictionaries to be serialized.
+
+    Returns:
+        bytes: The serialized data representing the input list of dictionaries.
+    """
     buffer = io.BytesIO()
 
     pickle.dump(dicts, buffer)
