df3893db4364f415c013125f08716f2aa6a8e4f7
Chris <chris79gp@outlook.com>
2024-06-02 21:20:08 +0100

    feat: add get_file_type returns a string indicating the file type from a file path returns None for unsupported files and files without an extension

--- src/get_file_type.py
+++ src/get_file_type.py
@@ -1,2 +1,20 @@
-def get_file_type(path):
-    pass
+import re
+
+
+def get_file_type(file_path):
+    extension_map = {
+        'csv': 'csv',
+        'json': 'json',
+        'pqt': 'parquet',
+        'parquet': 'parquet'
+    }
+
+    extension_pattern = r'\.(\w+)(?:\?.*|#.*)?$'
+    extension = re.search(extension_pattern, file_path)
+
+    if not extension:
+        return None
+
+    file_type = extension_map.get(extension.group(1))
+
+    return file_type
