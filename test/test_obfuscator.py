bae447d68cb44d57c54f1803a6d25637b91450ff
Chris <chris79gp@outlook.com>
2024-06-05 12:47:33 +0100

    test: update unit tests for obfuscator to test for get_data instead of get_file_type and handle_csv patches for get_file_type and handle_csv replaced with patch for get_data call assertions for get_file_type and handle_csv replaced with call assertion for get_data unit test for ValueError removed due to duplicate test in test_get_file_type

--- test/test_obfuscator.py
+++ test/test_obfuscator.py
@@ -24,17 +24,14 @@ class TestObfuscator(unittest.TestCase):
 
     @patch("src.obfuscator.serialise_dicts")
     @patch("src.obfuscator.obfuscate_fields")
-    @patch("src.obfuscator.handle_csv")
-    @patch("src.obfuscator.get_file_type")
+    @patch("src.obfuscator.get_data")
     def test_obfuscator_calls_helper_functions(
             self,
-            mock_get_file_type,
-            mock_handle_csv,
+            mock_get_data,
             mock_obfuscate_fields,
             mock_serialise_dicts
     ):
-        mock_get_file_type.return_value = 'csv'
-        mock_handle_csv.return_value = self.original_data
+        mock_get_data.return_value = self.original_data
         mock_obfuscate_fields.return_value = self.obfuscated_data
         mock_serialise_dicts.return_value = self.serialized_data
 
@@ -45,33 +42,12 @@ class TestObfuscator(unittest.TestCase):
 
         result = obfuscator(event)
 
-        mock_get_file_type.assert_called_once_with("s3://bucket/data/file.csv")
-        mock_handle_csv.assert_called_once_with("s3://bucket/data/file.csv")
+        mock_get_data.assert_called_once_with("s3://bucket/data/file.csv")
         mock_obfuscate_fields.assert_called_once_with(
             self.original_data, event["pii_fields"])
         mock_serialise_dicts.assert_called_once_with(self.obfuscated_data)
 
         assert result == self.serialized_data
-
-    @patch("src.obfuscator.get_file_type")
-    def test_obfuscator_handles_unsupported_file_type(
-            self,
-            mock_get_file_type
-    ):
-        mock_get_file_type.return_value = 'txt'
-
-        event = {
-            "file_to_obfuscate": "s3://bucket/data/file.txt",
-            "pii_fields": ["name", "email_address"]
-        }
-
-        with self.assertRaises(ValueError) as context:
-            obfuscator(event)
-
-        self.assertEqual(
-            str(context.exception),
-            "File type .txt is not supported."
-        )
 
 
 if __name__ == '__main__':
