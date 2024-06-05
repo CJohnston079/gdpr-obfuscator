f706804ec63049ce579b443d793586697038a545
Chris <chris79gp@outlook.com>
2024-06-05 12:34:04 +0100

    feat: add get_data get_data abstracts the logic determining file type and fetching data from obfuscator takes a "file_path" argument and returns a list of dictionaries from the file calls get_file_type to set the variable file_type calls handle_csv if file_type == csv raises a ValueError if the file type is not supported

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -1,2 +1,16 @@
-def get_data():
-    pass
+from src.utils.get_file_type import get_file_type
+from src.file_handlers.handle_csv import handle_csv
+
+
+def get_data(file_path):
+    file_type = get_file_type(file_path)
+
+    if file_type == 'csv':
+        data = handle_csv(file_path)
+        data = 'poo'
+    else:
+        raise ValueError(
+            f"File type .{file_type} is not supported."
+        )
+
+    return data
