9904f857b1cf6315d4a00c610a27cdf4942eddff
Chris <chris79gp@outlook.com>
2024-06-05 12:48:48 +0100

    refactor: replace get_file_type and handle_csv with get_data

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,5 +1,4 @@
-from src.file_handlers.handle_csv import handle_csv
-from src.utils.get_file_type import get_file_type
+from src.utils.get_data import get_data
 from src.utils.obfuscate_fields import obfuscate_fields
 from src.utils.serialise_dicts import serialise_dicts
 
@@ -8,15 +7,7 @@ def obfuscator(event):
     file_path = event["file_to_obfuscate"]
     fields_to_obfuscate = event["pii_fields"]
 
-    file_type = get_file_type(file_path)
-
-    if file_type == 'csv':
-        data = handle_csv(file_path)
-    else:
-        raise ValueError(
-            f"File type .{file_type} is not supported."
-        )
-
+    data = get_data(file_path)
     obfuscated_data = obfuscate_fields(data, fields_to_obfuscate)
     serialized_data = serialise_dicts(obfuscated_data)
 
