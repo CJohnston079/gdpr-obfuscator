3cba4df91373caa007d5d49fbd83d112430c8abf
Chris <chris79gp@outlook.com>
2024-06-05 12:31:45 +0100

    test: add unit tests for get_data test that get_file_type is called test that handle_csv is called when file_type == csv test that a ValueError is raised for unsupported file types

--- test/utils/test_get_data.py
+++ test/utils/test_get_data.py
@@ -1,6 +1,43 @@
+import unittest
+from unittest.mock import patch
 from src.utils.get_data import get_data
 
 
-class TestGetData():
-    def test_get_data(self):
-        pass
+class TestGetData(unittest.TestCase):
+    @patch("src.utils.get_data.handle_csv")
+    @patch("src.utils.get_data.get_file_type")
+    def test_get_data_calls_get_file_type(
+            self,
+            mock_get_file_type,
+            mock_handle_csv
+    ):
+        mock_get_file_type.return_value = 'csv'
+        result = get_data("s3://bucket/data/file.csv")
+        mock_get_file_type.assert_called_once_with("s3://bucket/data/file.csv")
+
+    @patch("src.utils.get_data.handle_csv")
+    @patch("src.utils.get_data.get_file_type")
+    def test_get_data_calls_handle_csv_when_file_type_is_csv(
+            self,
+            mock_get_file_type,
+            mock_handle_csv
+    ):
+        mock_get_file_type.return_value = 'csv'
+        result = get_data("s3://bucket/data/file.csv")
+        mock_handle_csv.assert_called_once_with("s3://bucket/data/file.csv")
+
+    @patch("src.utils.get_data.get_file_type")
+    def test_get_data_handles_unsupported_file_type(self, mock_get_file_type):
+        mock_get_file_type.return_value = 'txt'
+
+        with self.assertRaises(ValueError) as context:
+            get_data("s3://bucket/data/file.txt")
+
+        self.assertEqual(
+            str(context.exception),
+            "File type .txt is not supported."
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
