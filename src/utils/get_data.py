7aea05ef39cf2e36d4bd4f1ccc8c821c65780e98
Chris <chris79gp@outlook.com>
2024-06-05 12:37:41 +0100

    docs: add docstring for get_data

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -3,6 +3,21 @@ from src.file_handlers.handle_csv import handle_csv
 
 
 def get_data(file_path):
+    """
+    Reads the contents of a file and returns a list of dictionaries,
+
+    Args:
+        file_path (str): The path to the file to be read.
+
+    Returns:
+        list: A list of dictionaries representing the data in the file.
+              Each dictionary contains key-value pairs where the keys are
+              column headers and the values are the corresponding values
+              from each row.
+
+    Raises:
+        ValueError: If no function exists to handle the file_type
+    """
     file_type = get_file_type(file_path)
 
     if file_type == 'csv':
