030bd7637ae2cfd37812d76025670034757e0324
Chris <chris79gp@outlook.com>
2024-06-06 17:38:15 +0100

    docs: add docstring to handle_json

--- src/utils/file_handlers/handle_json.py
+++ src/utils/file_handlers/handle_json.py
@@ -4,6 +4,19 @@ import io
 
 
 def handle_json(file_path):
+    """
+    Reads the contents of a JSON file from an AWS S3 bucket and returns a list
+    of dictionaries.
+
+    Args:
+        file_path (str): The S3 bucket path to the JSON file to be read. The path
+            should be in the format 's3://bucket_name/path/to/file.json'.
+
+    Returns:
+        list:
+            A list of dictionaries representing the data in the JSON file.
+    """
+
     bucket_name = file_path.split('/')[2]
     key = '/'.join(file_path.split('/')[3:])
 
