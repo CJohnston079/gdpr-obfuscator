963e5597b8f4aba43b85cc59b038b7d38c3df985
Chris <chris79gp@outlook.com>
2024-06-06 17:37:57 +0100

    feat: add handle_json returns a list of dictionaries parsed from a .json file in an aws s3 bucket takes file_path as an argument returns an empty list when handling a blank file

--- src/utils/file_handlers/handle_json.py
+++ src/utils/file_handlers/handle_json.py
@@ -1,2 +1,15 @@
+import json
+import boto3
+import io
+
+
 def handle_json(file_path):
-    pass
+    bucket_name = file_path.split('/')[2]
+    key = '/'.join(file_path.split('/')[3:])
+
+    s3 = boto3.client("s3", region_name="eu-west-2")
+
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    content = response['Body'].read().decode('utf-8')
+
+    return json.loads(content) if content else []
