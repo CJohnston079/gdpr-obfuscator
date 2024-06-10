352ae3287a3248a3df3a6e439fd61465382a9b8a
Chris <chris79gp@outlook.com>
2024-06-10 14:16:31 +0100

    chore: add handle_parquet.py and test_handle_parquet.py

---
+++ src/utils/file_handlers/handle_parquet.py
@@ -0,0 +1,2 @@
+def handle_parquet(file_path):
+    pass
---
+++ test/utils/file_handlers/test_handle_parquet.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.utils.file_handlers.handle_parquet import handle_paruqet
+
+
+class TestHandleCSV(unittest.TestCase):
+    def test_returns_expected_data(self):
+        pass
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
