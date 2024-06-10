8e18ffd2662eb4ced7f8ae6cfd262a4c4d1c88d5
Chris <chris79gp@outlook.com>
2024-06-10 14:45:36 +0100

    feat: add handle_parquet function handle_parquet takes a file_path and returns a list of dicts the file_path should be in the format "s3://bucket_name/path/to/file.parquet" if the file is empty, handle_csv returns an empty list

--- src/utils/file_handlers/handle_parquet.py
+++ src/utils/file_handlers/handle_parquet.py
@@ -1,2 +1,19 @@
+import boto3
+import io
+import pyarrow.parquet as pq
+
+
 def handle_parquet(file_path):
-    pass
+    bucket_name = file_path.split('/')[2]
+    key = '/'.join(file_path.split('/')[3:])
+
+    s3 = boto3.client("s3", region_name="eu-west-2")
+
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    content = response['Body'].read()
+    file_content = io.BytesIO(content)
+
+    table = pq.read_table(file_content)
+    data = table.to_pandas().to_dict(orient='records')
+
+    return data
