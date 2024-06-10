f7dee6a55962e10ba404a5fc6e89996223bfb747
Chris <chris79gp@outlook.com>
2024-06-10 14:42:47 +0100

    test: add unit tests for handle_parquet test handle_parquet returns a list of dicts test the returned list of dicts is the expected length test the returned list of dicts contains the expected data test handle_parquet returns an empty list if the file is empty

--- test/utils/file_handlers/test_handle_parquet.py
+++ test/utils/file_handlers/test_handle_parquet.py
@@ -1,11 +1,68 @@
+import boto3
 import unittest
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pandas as pd
 
-from src.utils.file_handlers.handle_parquet import handle_paruqet
+from moto import mock_aws
+from src.utils.file_handlers.handle_parquet import handle_parquet
 
 
-class TestHandleCSV(unittest.TestCase):
+@mock_aws
+class TestHandleParquet(unittest.TestCase):
+    def setUp(self):
+        self.sample_data = [
+            {'name': 'George', 'age': 44, 'city': 'York'},
+            {'name': 'Lindsay', 'age': 40, 'city': 'Leeds'},
+            {'name': 'Michael', 'age': 37, 'city': 'Sheffield'}
+        ]
+
+        df = pd.DataFrame(self.sample_data)
+        table = pa.Table.from_pandas(df)
+        parquet_buffer = pa.BufferOutputStream()
+        pq.write_table(table, parquet_buffer)
+        parquet_data = parquet_buffer.getvalue().to_pybytes()
+
+        bucket_name = 'test-bucket'
+        s3 = boto3.client("s3", region_name="us-east-1")
+        s3.create_bucket(Bucket="test-bucket")
+        s3.put_object(
+            Bucket=bucket_name,
+            Key='test/test.parquet',
+            Body=parquet_data
+        )
+
+    def test_returns_list_of_dicts(self):
+        result = handle_parquet("s3://test-bucket/test/test.parquet")
+        self.assertIsInstance(result, list)
+        self.assertTrue(all(isinstance(row, dict) for row in result))
+
+    def test_returns_list_of_expected_length(self):
+        result = handle_parquet("s3://test-bucket/test/test.parquet")
+        self.assertEqual(len(result), 3)
+
+    def test_returns_empty_list_when_passed_empty_file(self):
+        empty_df = pd.DataFrame()
+        empty_table = pa.Table.from_pandas(empty_df)
+        empty_buffer = pa.BufferOutputStream()
+        pq.write_table(empty_table, empty_buffer)
+        empty_parquet_data = empty_buffer.getvalue().to_pybytes()
+
+        bucket_name = 'test-bucket'
+        s3 = boto3.client("s3", region_name="us-east-1")
+        s3.create_bucket(Bucket=bucket_name)
+        s3.put_object(
+            Bucket=bucket_name,
+            Key='test/empty-file.parquet',
+            Body=empty_parquet_data
+        )
+
+        result = handle_parquet("s3://test-bucket/test/empty-file.parquet")
+        self.assertEqual(result, [])
+
     def test_returns_expected_data(self):
-        pass
+        result = handle_parquet("s3://test-bucket/test/test.parquet")
+        self.assertEqual(result, self.sample_data)
 
 
 if __name__ == '__main__':  # pragma: no cover
