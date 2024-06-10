dbd35b12e40c925d5629b15c9952a31b37835aa8
Chris <chris79gp@outlook.com>
2024-06-10 21:55:48 +0100

    feat: add handle_xml() handle_xml takes a file_path argument and returns a list of dictionaries containing the data the structure of the xml file is expected to be <root><data_tag>...data</data_tag></root>

--- src/utils/file_handlers/handle_xml.py
+++ src/utils/file_handlers/handle_xml.py
@@ -1,2 +1,29 @@
+import xml.etree.ElementTree as ET
+import boto3
+import io
+
+
 def handle_xml(file_path):
-    pass
+    bucket_name = file_path.split('/')[2]
+    key = '/'.join(file_path.split('/')[3:])
+
+    s3 = boto3.client("s3", region_name="eu-west-2")
+
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    content = response['Body'].read().decode('utf-8')
+
+    if not content:
+        return []
+
+    root = ET.fromstring(content)
+    data_tag = root[0].tag
+
+    data = []
+
+    for element in root.findall(f'.//{data_tag}'):
+        item_data = {}
+        for child in element:
+            item_data[child.tag] = child.text
+        data.append(item_data)
+
+    return data
