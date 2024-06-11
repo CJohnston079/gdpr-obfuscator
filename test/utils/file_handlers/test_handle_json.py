5e1f4b919bf952812aa243c634701d7b66bed607
Chris <chris79gp@outlook.com>
2024-06-11 15:13:05 +0100

    test: add unit test for handle_json to handle deep data

--- test/utils/file_handlers/test_handle_json.py
+++ test/utils/file_handlers/test_handle_json.py
@@ -10,42 +10,82 @@ from src.utils.file_handlers.handle_json import handle_json
 @mock_aws
 class TestHandleJson(unittest.TestCase):
     def setUp(self):
-        self.sample_data = [
+        self.sample_shallow_data = [
             {'name': 'George', 'age': '44', 'city': 'York'},
             {'name': 'Lindsay', 'age': '40', 'city': 'Leeds'},
             {'name': 'Michael', 'age': '37', 'city': 'Sheffield'}
         ]
 
-        sample_json_data = json.dumps(self.sample_data)
+        self.sample_deep_data = [
+            {
+                'name': 'George',
+                'age': '44',
+                'city': 'York',
+                'contact': [
+                    {'type': 'email', 'value': 'george@bluthcompany.com'},
+                    {'type': 'phone', 'value': '01904 123456'}
+                ],
+            },
+            {
+                'name': 'Lindsay',
+                'age': '40',
+                'city': 'Leeds',
+                'contact': [
+                    {'type': 'email', 'value': 'lindsay@bluthcompany.com'},
+                    {'type': 'phone', 'value': '0113 123456'}
+                ],
+            },
+            {
+                'name': 'Michael',
+                'age': '37',
+                'city': 'Sheffield',
+                'contact': [
+                    {'email': 'michael@bluthcompany.com'},
+                    {'phone': '0114 123456'}
+                ],
+            }
+        ]
+
+        sample_shallow_json_data = json.dumps(self.sample_shallow_data)
+        sample_deep_json_data = json.dumps(self.sample_deep_data)
 
         bucket_name = 'test-bucket'
         s3 = boto3.client("s3", region_name="us-east-1")
         s3.create_bucket(Bucket="test-bucket")
-        s3.put_object(Bucket=bucket_name, Key='test/empty-file.json', Body="")
+        s3.put_object(Bucket=bucket_name, Key='test/test_empty.json', Body="")
         s3.put_object(
             Bucket=bucket_name,
-            Key='test/test.json',
-            Body=sample_json_data
+            Key='test/test-shallow.json',
+            Body=sample_shallow_json_data
+        )
+        s3.put_object(
+            Bucket=bucket_name,
+            Key='test/test-deep.json',
+            Body=sample_deep_json_data
         )
 
     def test_returns_list_of_dicts(self):
-        result = handle_json("s3://test-bucket/test/test.json")
+        result = handle_json("s3://test-bucket/test/test-shallow.json")
         self.assertIsInstance(result, list)
         self.assertTrue(all(isinstance(row, dict) for row in result))
 
     def test_returns_list_of_expected_length(self):
-        result = handle_json("s3://test-bucket/test/test.json")
+        result = handle_json("s3://test-bucket/test/test-shallow.json")
         self.assertEqual(len(result), 3)
 
     def test_returns_empty_list_when_passed_empty_file(self):
-        result = handle_json("s3://test-bucket/test/empty-file.json")
+        result = handle_json("s3://test-bucket/test/test_empty.json")
 
         print(result)
         self.assertEqual(result, [])
 
-    def test_returns_expected_data(self):
-        result = handle_json("s3://test-bucket/test/test.json")
-        self.assertEqual(result, self.sample_data)
+    def test_returns_expected_shallow_data(self):
+        result = handle_json("s3://test-bucket/test/test-shallow.json")
+        self.assertEqual(result, self.sample_shallow_data)
+
+    def test_returns_expected_deep_data(self):
+        result = handle_json("s3://test-bucket/test/test-deep.json")
+        self.assertEqual(result, self.sample_deep_data)
 
 
 if __name__ == '__main__':  # pragma: no cover
