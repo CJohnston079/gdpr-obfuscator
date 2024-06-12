a910576b84ba3a278f7c1323c6707d32694f7e57
Chris <chris79gp@outlook.com>
2024-06-12 21:16:05 +0100

    test: update unit tests for serialise_dicts to test shallow data, array-based deep data and object-based deep data

--- test/utils/test_serialise_dicts.py
+++ test/utils/test_serialise_dicts.py
@@ -5,8 +5,8 @@ from src.utils.serialise_dicts import serialise_dicts
 
 
 class TestSerialiseDicts:
-    def test_serialise_dicts_does_not_mutate_input(self):
-        original_dicts = [{"a": 1, "b": 2}, {"c": 3, "d": 4}]
+    def test_serialise_dicts_does_not_mutate_input(self, ts_shallow_data):
+        original_dicts, _ = ts_shallow_data
         copied_dicts = original_dicts.copy()
 
         serialised_data = serialise_dicts(original_dicts)
@@ -27,35 +27,38 @@ class TestSerialiseDicts:
 
         assert deserialised_data == dicts
 
-    def test_serialises_single_dict(self):
-        dicts = [{"a": 1, "b": 2}]
+    def test_serialises_shallow_data(self, ts_shallow_data):
+        data, _ = ts_shallow_data
 
-        serialised_data = serialise_dicts(dicts)
-
-        buffer = io.BytesIO(serialised_data)
-        deserialised_data = pickle.load(buffer)
-
-        assert deserialised_data == dicts
-
-    def test_serialises_multiple_dicts(self):
-        dicts = [{"a": 1, "b": 2}, {"c": 3, "d": 4}]
-
-        serialised_data = serialise_dicts(dicts)
+        serialised_data = serialise_dicts(data)
 
         buffer = io.BytesIO(serialised_data)
         deserialised_data = pickle.load(buffer)
 
-        assert deserialised_data == dicts
+        assert deserialised_data == data
 
-    def test_serialise_nested_dicts(self):
-        dicts = [{"a": {"b": 1, "c": 2}}, {"d": {"e": 3, "f": 4}}]
+    def test_serialises_deep_array_based_data(self, ts_deep_array_based_data):
+        data, _ = ts_deep_array_based_data
 
-        serialised_data = serialise_dicts(dicts)
+        serialised_data = serialise_dicts(data)
 
         buffer = io.BytesIO(serialised_data)
         deserialised_data = pickle.load(buffer)
 
-        assert deserialised_data == dicts
+        assert deserialised_data == data
+
+    def test_serialises_deep_object_based_data(
+        self,
+        ts_deep_object_based_data
+    ):
+        data, _ = ts_deep_object_based_data
+
+        serialised_data = serialise_dicts(data)
+
+        buffer = io.BytesIO(serialised_data)
+        deserialised_data = pickle.load(buffer)
+
+        assert deserialised_data == data
 
 
 if __name__ == '__main__':  # pragma: no cover
