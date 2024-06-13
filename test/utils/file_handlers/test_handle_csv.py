9f6e9b358e588491da25ef61b906bb983acbdf17
Chris <chris79gp@outlook.com>
2024-06-13 15:47:20 +0100

    refactor: refactor test_handle_csv to use pytest fixtures instead of unittest setUp()

--- test/utils/file_handlers/test_handle_csv.py
+++ test/utils/file_handlers/test_handle_csv.py
@@ -1,54 +1,43 @@
 import boto3
-import unittest
+import pytest
 
-from moto import mock_aws
-from unittest.mock import patch, mock_open
 from src.utils.file_handlers.handle_csv import handle_csv
 
 
-@mock_aws
-class TestHandleCSV(unittest.TestCase):
-    def setUp(self):
-        self.sample_data = [
-            {"name": "George", "age": "44", "city": "York"},
-            {"name": "Lindsay", "age": "40", "city": "Leeds"},
-            {"name": "Michael", "age": "37", "city": "Sheffield"}
-        ]
+class TestHandleCSV():
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_method(self, s3_bucket, ts_shallow_data):
+        s3, bucket_name = s3_bucket
+        data, _ = ts_shallow_data
 
-        sample_csv_data = (
-            "name,age,city\n"
-            "George,44,York\n"
-            "Lindsay,40,Leeds\n"
-            "Michael,37,Sheffield"
-        )
+        headers = data[0].keys()
+        rows = [",".join([row[key] for key in headers]) for row in data]
+        csv_data = ",".join(headers) + "\n" + "\n".join(rows)
 
-        bucket_name = "test-bucket"
-        s3 = boto3.client("s3", region_name="us-east-1")
-        s3.create_bucket(Bucket="test-bucket")
-        s3.put_object(Bucket=bucket_name, Key="test/empty-file.csv", Body="")
+        s3.put_object(Bucket=bucket_name, Key="dir/empty-file.csv", Body="")
         s3.put_object(
             Bucket=bucket_name,
-            Key="test/test.csv",
-            Body=sample_csv_data
+            Key="dir/test-csv.csv",
+            Body=csv_data
         )
 
-    def test_returns_list_of_dicts(self):
-        result = handle_csv("s3://test-bucket/test/test.csv")
-        self.assertIsInstance(result, list)
-        self.assertTrue(all(isinstance(row, dict) for row in result))
+        return data
 
-    def test_returns_list_of_expected_length(self):
-        result = handle_csv("s3://test-bucket/test/test.csv")
-        self.assertEqual(len(result), 3)
+    def test_returns_list_of_dicts(self):
+        result = handle_csv("s3://test-bucket/dir/test-csv.csv")
+        assert isinstance(result, list)
+        assert all(isinstance(row, dict) for row in result)
+
+    def test_returns_list_of_expected_length(self, setup_method):
+        data = setup_method
+        result = handle_csv("s3://test-bucket/dir/test-csv.csv")
+        assert len(result) == len(data)
 
     def test_returns_empty_list_when_passed_empty_file(self):
-        result = handle_csv("s3://test-bucket/test/empty-file.csv")
-        self.assertEqual(result, [])
+        result = handle_csv("s3://test-bucket/dir/empty-file.csv")
+        assert result == []
 
-    def test_returns_expected_data(self):
-        result = handle_csv("s3://test-bucket/test/test.csv")
-        self.assertEqual(result, self.sample_data)
-
-
-if __name__ == "__main__":  # pragma: no cover
-    unittest.main()
+    def test_returns_expected_data(self, setup_method):
+        data = setup_method
+        result = handle_csv("s3://test-bucket/dir/test-csv.csv")
+        assert result == data
