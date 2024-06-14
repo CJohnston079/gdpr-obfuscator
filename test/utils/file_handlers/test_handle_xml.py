bc3bcadae7c2fe6229ca4ccc4838ecd2210d4e1f
Chris <chris79gp@outlook.com>
2024-06-14 09:32:33 +0100

    refactor: revise fixture requests in test_handle_xml

--- test/utils/file_handlers/test_handle_xml.py
+++ test/utils/file_handlers/test_handle_xml.py
@@ -9,46 +9,44 @@ class TestHandleXML():
     def set_up_s3_data(self, s3_bucket, test_xml_data):
         s3, bucket_name = s3_bucket
 
-        sample_shallow_data = test_xml_data["shallow_object_based"]
-        sample_shallow_xml_data = test_xml_data["shallow_xml_str"]
-        sample_deep_data = test_xml_data["deep_object_based"]
-        sample_deep_xml_data = test_xml_data["deep_xml_str"]
+        shallow_data = test_xml_data["shallow_object_based"]
+        shallow_xml_data = test_xml_data["shallow_xml_str"]
+        deep_data = test_xml_data["deep_object_based"]
+        deep_xml_data = test_xml_data["deep_xml_str"]
 
         s3.put_object(Bucket=bucket_name, Key="dir/empty-file.xml", Body="")
         s3.put_object(
             Bucket=bucket_name,
             Key="dir/shallow-data.xml",
-            Body=sample_shallow_xml_data
+            Body=shallow_xml_data
         )
         s3.put_object(
             Bucket=bucket_name,
             Key="dir/deep-data.xml",
-            Body=sample_deep_xml_data
+            Body=deep_xml_data
         )
-
-        return sample_shallow_data, sample_deep_data
 
     def test_returns_list_of_dicts(self):
         result = handle_xml("s3://test-bucket/dir/shallow-data.xml")
         assert isinstance(result, list)
         assert all(isinstance(row, dict) for row in result)
 
-    def test_returns_list_of_expected_length(self, set_up_s3_data):
-        sample_shallow_data, _ = set_up_s3_data
+    def test_returns_list_of_expected_length(self, test_xml_data):
+        shallow_data = test_xml_data["shallow_object_based"]
         result = handle_xml("s3://test-bucket/dir/shallow-data.xml")
-        assert len(result) == len(sample_shallow_data)
+        assert len(result) == len(shallow_data)
 
     def test_returns_empty_list_when_passed_empty_file(self):
         result = handle_xml("s3://test-bucket/dir/empty-file.xml")
         assert result == []
 
-    def test_returns_expected_shallow_data(self, set_up_s3_data):
-        sample_shallow_data, _ = set_up_s3_data
+    def test_returns_expected_shallow_data(self, test_xml_data):
+        shallow_data = test_xml_data["shallow_object_based"]
         result = handle_xml("s3://test-bucket/dir/shallow-data.xml")
 
-        assert result == sample_shallow_data
+        assert result == shallow_data
 
-    def test_returns_expected_deep_data(self, set_up_s3_data):
-        _, sample_deep_data = set_up_s3_data
+    def test_returns_expected_deep_data(self, test_xml_data):
+        deep_data = test_xml_data["deep_object_based"]
         result = handle_xml("s3://test-bucket/dir/deep-data.xml")
-        assert result == sample_deep_data
+        assert result == deep_data
