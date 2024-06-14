e5f913a96f12784cfed2cc9b8ce93c13c15c08fb
Chris <chris79gp@outlook.com>
2024-06-14 21:56:35 +0100

    feat: raise ValueError for unsupported file types in obfuscator

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -4,11 +4,15 @@ from src.utils.serialise_dicts import serialise_dicts
 
 
 def obfuscator(event):
-    file_path = event["file_to_obfuscate"]
-    fields_to_obfuscate = event["pii_fields"]
+    try:
+        file_path = event["file_to_obfuscate"]
+        fields_to_obfuscate = event["pii_fields"]
 
-    data = get_data(file_path)
-    obfuscated_data = obfuscate_fields(data, fields_to_obfuscate)
-    serialized_data = serialise_dicts(obfuscated_data)
+        data = get_data(file_path)
+        obfuscated_data = obfuscate_fields(data, fields_to_obfuscate)
+        serialized_data = serialise_dicts(obfuscated_data)
 
-    return serialized_data
+        return serialized_data
+
+    except ValueError as ve:
+        raise ValueError(ve) from ve
