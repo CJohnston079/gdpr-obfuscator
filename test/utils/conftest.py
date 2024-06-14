9a99213d5ecad03f46abb27e0b4188ec2dfaea5f
Chris <chris79gp@outlook.com>
2024-06-14 11:21:12 +0100

    chore: remove redundant data fixtures fron utils/conftest.py and remove redundant data generators from data_generation.py

--- test/utils/conftest.py
+++ test/utils/conftest.py
@@ -1,10 +1,5 @@
 import pytest
-from data_generation import (
-    generate_data,
-    generate_shallow_data_old,
-    generate_deep_array_based_data,
-    generate_deep_object_based_data
-)
+from data_generation import generate_data
 
 
 @pytest.fixture(scope="session")
@@ -39,23 +34,3 @@ def test_xml_data():
     data["deep_xml_str"] = f'<root>{data["deep_xml_str"]}</root>'
 
     return data
-
-
-@pytest.fixture(scope="class")
-def test_shallow_object_based_data(generate_shallow_data):
-    return generate_shallow_data["shallow_object_based_obfuscated"]
-
-
-@pytest.fixture(scope="class")
-def ts_shallow_data():
-    return generate_shallow_data_old()
-
-
-@pytest.fixture(scope="class")
-def ts_deep_array_based_data():
-    return generate_deep_array_based_data()
-
-
-@pytest.fixture(scope="class")
-def ts_deep_object_based_data():
-    return generate_deep_object_based_data()
