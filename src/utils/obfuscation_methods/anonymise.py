648784db54f124c9b4481bc31c6a703d0dc85dcb
Chris <chris79gp@outlook.com>
2024-06-21 09:53:55 +0100

    docs: add docstring for `anonymise`

--- src/utils/obfuscation_methods/anonymise.py
+++ src/utils/obfuscation_methods/anonymise.py
@@ -63,6 +63,21 @@ pii_fields = {
 
 
 def anonymise(field, options):
+    """
+    Returns an randomly generated value intended to be used as an obfuscation
+    for a field in a data set.
+
+    Args:
+        field (string): The field to be obfuscated, used to look up an
+            appropriate value in pii_fields.
+        options (dict): A dictionary containing obfuscation options. If the key
+            "token" is in options, then the value will be returned in the
+            event that field is not found in pii_fields.
+
+    Returns:
+        str: A randomly generated value from pii_fields, or a token if a value
+            is not found in pii_fields.
+    """
     cleaned_field = re.sub(r"[ \-_]", "", field).lower()
     value = pii_fields.get(cleaned_field, tokenise(field, options))
     return value
