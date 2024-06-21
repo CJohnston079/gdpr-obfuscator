681a24c48ed657f23c423aeb69d0c622a9d4b7e8
Chris <chris79gp@outlook.com>
2024-06-21 09:06:34 +0100

    test: add unit tests for `anonymise` test `anonymise` generates appropriate data for each field test `anonymise` can look up fields that have spaces, or are kebab, snake, camel or pascal case test `anonymise` returns a token if field is not in `pii_fields`

--- src/utils/obfuscation_methods/anonymise.py
+++ src/utils/obfuscation_methods/anonymise.py
@@ -1,2 +1,60 @@
+import random
+
+from faker import Faker
+
+faker = Faker("en_GB")
+
+
+pii_fields = {
+    "name": faker.name(),
+    "firstname": faker.first_name(),
+    "lastname": faker.last_name(),
+    "surname": faker.last_name(),
+    "email": faker.email(),
+    "emailaddress": faker.email(),
+    "gender": random.choice(["M", "F", "TG", "NB", "AG", "BI"]),
+    "age": lambda: random.randint(18, 66),
+    "birthday": faker.date_of_birth(),
+    "dateofbirth": faker.date_of_birth(),
+    "dob": faker.date_of_birth(),
+    "phone": faker.phone_number(),
+    "mobile": faker.phone_number(),
+    "landline": faker.phone_number(),
+    "workphone": faker.phone_number(),
+    "hometelephone": faker.phone_number(),
+    "worktelephone": faker.phone_number(),
+    "mobilephone": faker.phone_number(),
+    "homephone": faker.phone_number(),
+    "phonenumber": faker.phone_number(),
+    "address": faker.address(),
+    "housenumber": faker.building_number(),
+    "homeaddress": faker.address(),
+    "workaddress": faker.address(),
+    "buildingnumber": faker.building_number(),
+    "street": faker.street_address(),
+    "streetaddress": faker.address(),
+    "town": faker.city(),
+    "city": faker.city(),
+    "county": faker.county(),
+    "postcode": faker.postcode(),
+    "nin": faker.ssn(),
+    "ssn": faker.ssn(),
+    "ninumber": faker.ssn(),
+    "nationalinsurancenumber": faker.ssn(),
+    "socialsecuritynumber": faker.ssn(),
+    "cardnumber": faker.credit_card_number(),
+    "debitcard": faker.credit_card_number(),
+    "creditcard": faker.credit_card_number(),
+    "creditcardnumber": faker.credit_card_number(),
+    "debitcardnumber": faker.credit_card_number(),
+    "ip": faker.ipv4(),
+    "ipaddress": faker.ipv4(),
+    "cookieid": faker.uuid4(),
+    "advertisingidentifier": faker.uuid4(),
+    "mobilelocationdata": faker.location_on_land(),
+    "locationdata": faker.location_on_land(),
+}
+
+
 def anonymise(field, options):
     pass
--- test/utils/obfuscation_methods/test_obfuscation_methods.py
+++ test/utils/obfuscation_methods/test_obfuscation_methods.py
@@ -1,3 +1,7 @@
+import pytest
+
+from src.utils.obfuscation_methods.anonymise import anonymise
+from src.utils.obfuscation_methods.anonymise import pii_fields
 from src.utils.obfuscation_methods.tokenise import tokenise
 
 
@@ -9,6 +13,35 @@ class TestTokenise:
         assert tokenise("name", {"token": "custom-token"}) == "custom-token"
 
 
+@pytest.mark.xfail
 class TestAnonymise:
-    def test_anonymise(self):
-        pass
+    @pytest.mark.parametrize(
+        "field, expected",
+        [
+            ("name", pii_fields["name"]),
+            ("birthday", pii_fields["birthday"]),
+            ("homephone", pii_fields["homephone"]),
+            ("address", pii_fields["address"]),
+            ("postcode", pii_fields["postcode"]),
+            ("email", pii_fields["email"]),
+        ],
+    )
+    def test_generates_data(self, field, expected):
+        assert anonymise(field, {}) == expected
+
+    @pytest.mark.parametrize(
+        "field, expected",
+        [
+            ("first name", pii_fields["firstname"]),
+            ("first-name", pii_fields["firstname"]),
+            ("first_name", pii_fields["firstname"]),
+            ("firstName", pii_fields["firstname"]),
+            ("FirstName", pii_fields["firstname"]),
+        ],
+    )
+    def test_works_for_camel_snake_and_kebab_case(self, field, expected):
+        assert anonymise(field, {}) == expected
+
+    def test_returns_token_if_pii_field_not_found(self):
+        assert anonymise("unexpected_field", {}) == "***"
+        assert anonymise("unexpected_field", {"token": "---"}) == "---"
