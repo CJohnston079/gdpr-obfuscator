15cebfbab0faf3f5b013c134fc36006fb6c78ebc
Chris <chris79gp@outlook.com>
2024-06-25 08:28:55 +0100

    feat: add `mask` obfuscation method `mask` returns a string of tokens the same length as `options["_val"] the token will be a single asterisk (`*`) by default unless one is specified in options["token"] if `options["_val"]` is `None` then `mask` will return a single token

--- src/utils/obfuscation_methods/mask.py
+++ src/utils/obfuscation_methods/mask.py
@@ -1,2 +1,20 @@
 def mask(field, options):
-    pass
+    """
+    Returns a token intended to be used as an obfuscation for a field in a data
+    set. The token is a triple asterisk ("***") by default.
+
+    Args:
+        field (string): The field to be obfuscated.
+        options (dict): A dictionary containing obfuscation options. It should
+            containt the key "_val" whose length determines the number of
+            repetitions of the token. If val is None then the function will
+            return a single token. If the key "token" is in options, then this
+            will be used as the token.
+
+    Returns:
+        str: A string of repeated tokens, corresponding to the length of
+            options["val"].
+    """
+    mask_length = len(options.get("_val") or "_")
+    token = options.get("token", "*")
+    return token * mask_length
